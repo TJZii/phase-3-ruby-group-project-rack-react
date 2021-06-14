ddb28dacb578e904c208211c694413e0c2ac2f3c
ziitania <tjhaweszii@gmail.com>
2021-06-14 01:37:20 -0700

    basics done, add more over rest of week

--- frontend/src/containers/GameForm.js
+++ frontend/src/containers/GameForm.js
@@ -20,9 +20,9 @@ class GameForm extends Component {
     render() {
         return (
             <div>
-                <h2>{props.game.name}</h2>
+                <h2>{this.props.game.name}</h2>
                 {/* {gameEditorFlag ? <GameEditor game={games.game} toggleForm={() => setGameEditorFlag(false) } editThisGame={games.editThisGame}/> : <button onClick={() => setGameEditorFlag(true)}>Game Settings</button>} */}
-                <button onChange={props.deleteThisGame(props.game.id)}>Delete</button>
+                <button onChange={this.props.deleteThisGame(this.props.game.id)}>Delete</button>
                 <form onSubmit={this.handleSubmit}>
                     <label>Name</label>
                     <br/>
